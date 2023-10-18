fab0a3a30a501fab44e85588978aa3c78fade82b
Miguel <blank0010b@gmail.com>
2023-10-18 18:46:45 +0100

    New gates

--- proj_2/arithmetic_logic.py
+++ proj_2/arithmetic_logic.py
@@ -2,31 +2,56 @@
 
 from collections import deque
 from logic_gates import *
+import copy
 
-def half_adder(a:int , b:int) -> tuple[int, int]: return (a ^ b, a & b)
+def half_adder(a:int , b:int) -> tuple[int, int]: return (Gate(a=a,b=b).xor_gate(), Gate(a=a,b=b).and_gate())
+
+def half_subtractor(a:int , b:int) -> tuple[int, int]: return (Gate(a=a,b=b).xor_gate(), Gate(a=not a,b=b).and_gate())
 
 def full_adder(a:int, b:int, c:int) -> tuple[int, int]:
 
    s0, c0 = half_adder(a, b)
 
-   s1, c1 = half_adder(c, s0)
+   s1, c1 = half_adder(s0, c)
 
-   return (s1, c1 | c0)
+   return (s1, c0 | c1)
+
+def full_subtractor(a:int, b:int, c:int) -> tuple[int, int]:
+
+    d0, b0 = half_subtractor(a, b)
+
+    d1, b1 = half_subtractor(d0, c)
+
+    return (d1, b0 | b1)
 
 def add16(a:list[int], b:list[int]) -> list[int]:
 
     #TODO: Maybe there is an alternative to the disgusting int cast
-    r = deque()
-    c_s, c_c = half_adder(int(a[-1]), int(b[-1]))
+    c_s, c_c = half_adder(int(a[-1]), int(b[-1])); r = deque()
     r.appendleft(c_s)
     
     for i in range(len(a) - 2, -1, -1):
         c_s, c_c = full_adder(int(a[i]), int(b[i]), c_c)
         r.appendleft(c_s)
 
-    print('\n',a,'\n',b)    
     if(c_c): r.appendleft(c_c)
 
     return [*r]
 
+def sub16(a:list[int], b:list[int]) -> list[int]:
+
+    c_d, c_b = half_subtractor(int(a[-1]), int(b[-1])); r = deque()
+    r.appendleft(c_d)
     
+    for i in range(len(a) - 2, -1, -1):
+        c_d, c_b = full_subtractor(int(a[i]), int(b[i]), c_b)
+        r.appendleft(c_d)
+
+    if(c_b): r.appendleft(c_b)
+
+    return [*r]
+
+
+def inc16(a:list[int]) -> list[int]: b = [0] * len(a); b[-1] = 1; return add16(a, b)
+
+def dec16(a:list[int]) -> list[int]: b = [0] * len(a); b[-1] = 1; return sub16(a, b)
